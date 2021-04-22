dff017cd6795b57cf546dca797d806169a5ec56b
João Victor de Paula <jv.depaula@gmail.com>
2021-04-22 13:34:42 -0300

    initial commit

---
+++ PeakInvest.API/WebApplication1/Controllers/FinanceController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PeakInvest.API.Models;
+
+namespace PeakInvest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinanceController : ControllerBase
+    {
+        const byte percentage = 5;
+
+        [HttpPost]
+        public double Calculate(Installment model)
+        {
+            double total = (model.TotalValue * model.Installments);
+            total += total * (percentage / 100d);
+            return total;
+        }
+
+        [HttpGet("{id}")]
+        public KeyValuePair<int, string> GetCustomer(int id)
+        {
+            var list = new List<KeyValuePair<int, string>>
+            {
+                new KeyValuePair<int, string>(1, "João"),
+                new KeyValuePair<int, string>(2, "Maria"),
+                new KeyValuePair<int, string>(3, "Ana")
+            };
+
+            return list.FirstOrDefault(x => x.Key == id);
+        }
+    }
+}
---
+++ PeakInvest.API/WebApplication1/Models/Installment.cs
@@ -0,0 +1,8 @@
+﻿namespace PeakInvest.API.Models
+{
+    public class Installment
+    {
+        public double TotalValue { get; set; }
+        public int Installments { get; set; }
+    }
+}
